3c1c68562db18c0f6c8c17db017652c78dfd5d48
Maksym Hurynovych <aviard1208@gmail.com>
2021-11-29 19:24:00 +0200

    app to convert into kebab-case-style

--- KebabCaseConvrter/Program.cs
+++ KebabCaseConvrter/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-
+using System.Text;
 namespace KebabCaseConvrter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your text: ");
+            string regularText = Console.ReadLine();
+
+            string[] words = regularText.Split(new char[] { ',', ' ', '.', ';', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            var StringBuilder = new StringBuilder();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                regularText = words[i];
+                StringBuilder.Append(words[i]).Append("-");
+                continue;
+            }
+
+            string hyphenatedText = StringBuilder.ToString();
+            int lastHyphen = hyphenatedText.Length - 1;
+            string kebabCase = hyphenatedText.Remove(lastHyphen);
+
+            Console.WriteLine(kebabCase.ToLower());
         }
     }
 }
